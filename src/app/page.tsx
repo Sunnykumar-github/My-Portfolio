ed0d826de23f049d88c177cf0123b389fe327e93
Sunny Kumar <oneplusharry@gmail.com>
2025-06-25 10:15:29 +0000

    September 9, Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+
 import { Briefcase, Code, Github, GraduationCap, Linkedin, Mail, Twitter, Lightbulb, MessageSquareText, Puzzle, Users } from 'lucide-react';
 import Link from 'next/link';
 import { ContactForm } from '@/components/contact-form';
@@ -9,6 +10,7 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import Image from 'next/image';
 import { AnimatedOnScroll } from '@/components/animated-on-scroll';
+import { cn } from '@/lib/utils';
 
 const ReactIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-11.5 -10.23174 23 20.46348" className="h-full w-full">
@@ -30,17 +32,15 @@ const NextjsIcon = () => (
 )
 
 const JavaScriptIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-full w-full">
-        <rect width="24" height="24" rx="3" ry="3" fill="#F7DF1E"/>
-        <path d="M7.76 17.2V8.71H10.88C11.66 8.71 12.23 8.79 12.59 8.95C12.96 9.11 13.23 9.38 13.41 9.76C13.59 10.13 13.68 10.58 13.68 11.1C13.68 11.63 13.58 12.08 13.38 12.45C13.18 12.82 12.9 13.1 12.54 13.28C12.18 13.46 11.72 13.55 11.16 13.55H9.42V17.2H7.76ZM9.42 12.22H11C11.4 12.22 11.7 12.16 11.9 12.04C12.1 11.92 12.2 11.73 12.2 11.47C12.2 11.21 12.11 11.02 11.93 10.9C11.75 10.78 11.47 10.72 11.09 10.72H9.42V12.22ZM15.42 17.38C14.7 17.38 14.12 17.23 13.68 16.93C13.24 16.63 12.95 16.2 12.81 15.64H14.41C14.44 15.86 14.54 16.03 14.71 16.15C14.88 16.27 15.11 16.33 15.39 16.33C15.81 16.33 16.02 16.22 16.02 15.99C16.02 15.84 15.95 15.72 15.81 15.63C15.67 15.54 15.4 15.44 14.98 15.33L14.43 15.2C13.89 15.06 13.46 14.84 13.14 14.54C12.82 14.24 12.66 13.83 12.66 13.31C12.66 12.86 12.79 12.48 13.05 12.17C13.31 11.86 13.69 11.65 14.19 11.55C14.69 11.45 15.23 11.4 15.81 11.4C16.45 11.4 16.98 11.53 17.4 11.79C17.82 12.05 18.06 12.43 18.12 12.93H16.52C16.48 12.64 16.34 12.45 16.1 12.36C15.86 12.27 15.58 12.22 15.26 12.22C14.9 12.22 14.72 12.3 14.72 12.46C14.72 12.58 14.77 12.68 14.87 12.76C14.97 12.84 15.15 12.92 15.41 12.99L16.13 13.18C16.85 13.38 17.36 13.66 17.66 14.02C17.96 14.38 18.11 14.84 18.11 15.4C18.11 15.96 17.96 16.43 17.66 16.81C17.36 17.19 16.9 17.38 16.28 17.38H15.42Z" fill="#000"/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-full w-full" fill="currentColor">
+        <path d="M7.76 17.2V8.71H10.88C11.66 8.71 12.23 8.79 12.59 8.95C12.96 9.11 13.23 9.38 13.41 9.76C13.59 10.13 13.68 10.58 13.68 11.1C13.68 11.63 13.58 12.08 13.38 12.45C13.18 12.82 12.9 13.1 12.54 13.28C12.18 13.46 11.72 13.55 11.16 13.55H9.42V17.2H7.76ZM9.42 12.22H11C11.4 12.22 11.7 12.16 11.9 12.04C12.1 11.92 12.2 11.73 12.2 11.47C12.2 11.21 12.11 11.02 11.93 10.9C11.75 10.78 11.47 10.72 11.09 10.72H9.42V12.22ZM15.42 17.38C14.7 17.38 14.12 17.23 13.68 16.93C13.24 16.63 12.95 16.2 12.81 15.64H14.41C14.44 15.86 14.54 16.03 14.71 16.15C14.88 16.27 15.11 16.33 15.39 16.33C15.81 16.33 16.02 16.22 16.02 15.99C16.02 15.84 15.95 15.72 15.81 15.63C15.67 15.54 15.4 15.44 14.98 15.33L14.43 15.2C13.89 15.06 13.46 14.84 13.14 14.54C12.82 14.24 12.66 13.83 12.66 13.31C12.66 12.86 12.79 12.48 13.05 12.17C13.31 11.86 13.69 11.65 14.19 11.55C14.69 11.45 15.23 11.4 15.81 11.4C16.45 11.4 16.98 11.53 17.4 11.79C17.82 12.05 18.06 12.43 18.12 12.93H16.52C16.48 12.64 16.34 12.45 16.1 12.36C15.86 12.27 15.58 12.22 15.26 12.22C14.9 12.22 14.72 12.3 14.72 12.46C14.72 12.58 14.77 12.68 14.87 12.76C14.97 12.84 15.15 12.92 15.41 12.99L16.13 13.18C16.85 13.38 17.36 13.66 17.66 14.02C17.96 14.38 18.11 14.84 18.11 15.4C18.11 15.96 17.96 16.43 17.66 16.81C17.36 17.19 16.9 17.38 16.28 17.38H15.42Z" />
     </svg>
 )
 
 const TypeScriptIcon = () => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-full w-full">
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-full w-full" fill="currentColor">
         <title>TypeScript</title>
-        <path d="M1.5 0h21v21h-21z" fill="currentColor"/>
-        <path d="M12.288 15.328h2.64v-1.056h-1.056v-3.3h1.056v-1.056h-2.64v1.056h1.056v3.3h-1.056zm-3.972-.348c.42 0 .756-.156.996-.456l.816.66c-.396.504-.96.792-1.812.792-.612 0-1.116-.168-1.524-.504s-.612-.78-.612-1.32c0-.528.204-1.008.612-1.44s.936-.648 1.596-.648c.876 0 1.488.42 1.836 1.044l-.9.504c-.216-.432-.552-.648-.996-.648-.324 0-.588.108-.792.324s-.300.5-.300.864c0 .36.108.636.324.828s.456.288.756.288m10.824-2.544c.576 0 1.044.18 1.404.528s.54.792.54 1.332c0 .552-.18 1.01-.54 1.368s-.828.54-1.404.54c-.564 0-1.032-.18-1.404-.54s-.564-.816-.564-1.368c0-.54.18-1 .552-1.344s.828-.516 1.416-.516m0 5.4c1.332 0 2.292-.372 2.892-1.116s.9-1.74.9-2.988c0-1.26-.3-2.256-.9-2.988s-1.56-1.104-2.892-1.104c-1.344 0-2.316.36-2.916 1.08s-.9 1.716-.9 2.964c0 1.296.3 2.292.9 3.012s1.572 1.08 2.916 1.08" fill="hsl(var(--background))"/>
+        <path d="M12.288 15.328h2.64v-1.056h-1.056v-3.3h1.056v-1.056h-2.64v1.056h1.056v3.3h-1.056zm-3.972-.348c.42 0 .756-.156.996-.456l.816.66c-.396.504-.96.792-1.812.792-.612 0-1.116-.168-1.524-.504s-.612-.78-.612-1.32c0-.528.204-1.008.612-1.44s.936-.648 1.596-.648c.876 0 1.488.42 1.836 1.044l-.9.504c-.216-.432-.552-.648-.996-.648-.324 0-.588.108-.792.324s-.300.5-.300.864c0 .36.108.636.324.828s.456.288.756.288m10.824-2.544c.576 0 1.044.18 1.404.528s.54.792.54 1.332c0 .552-.18 1.01-.54 1.368s-.828.54-1.404.54c-.564 0-1.032-.18-1.404-.54s-.564-.816-.564-1.368c0-.54.18-1 .552-1.344s.828-.516 1.416-.516m0 5.4c1.332 0 2.292-.372 2.892-1.116s.9-1.74.9-2.988c0-1.26-.3-2.256-.9-2.988s-1.56-1.104-2.892-1.104c-1.344 0-2.316.36-2.916 1.08s-.9 1.716-.9 2.964c0 1.296.3 2.292.9 3.012s1.572 1.08 2.916 1.08" />
     </svg>
 )
 
@@ -72,10 +72,7 @@ const PostgreSqlIcon = () => (
 )
 
 const MongoDbIcon = () => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-full w-full" fill="currentColor">
-        <title>MongoDB</title>
-        <path d="M12.33,23.37a5.2,5.2,0,0,1-1.89-.36,10.6,10.6,0,0,1-5.32-4.15,13.23,13.23,0,0,1-2.94-7.44,13.43,13.43,0,0,1,2.83-8.86,10.3,10.3,0,0,1,8.35-4.14A10.23,10.23,0,0,1,21.8,2.58a13.33,13.33,0,0,1,2.81,9,13.2,13.2,0,0,1-2.82,8.91,10.33,10.33,0,0,1-8.4,4.14,6.33,6.33,0,0,1-1.06-.11Zm-1-10.45a.72.72,0,0,0,0,1.43c2.3,0,2.3-2.42,0-2.42a.72.72,0,0,0-.71.71.72.72,0,0,0,.71.72,1,1,0,0,1,1,1,.71.71,0,0,0,.71.72.72.72,0,0,0,.72-.72c0-2.3-2.3-2.3-2.42,0Z"/>
-    </svg>
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-full w-full" fill="currentColor"><title>MongoDB</title><path d="M15.93 24a8.17 8.17 0 01-6.42-3.09c-3.1-3.6-3.72-8.37-1.86-12.28a10.42 10.42 0 019-6.39c2.34 0 4.56.78 6.39 2.25 1.2 1.05 2.1 2.49 2.64 4.14.75 2.52.39 5.25-1.08 7.53-1.2 1.95-3.12 3.51-5.46 4.35-1.8.69-3.75 1.05-5.73 1.05a8.23 8.23 0 01-1.5-.12zm-3.6-11.64c1.26-1.08 1.89-2.58 1.53-4.08-.39-1.5-1.5-2.67-2.94-3-1.44-.39-3.03.12-4.02 1.2-.99 1.11-1.11 2.7-0.33 4.02.72 1.32 2.13 2.19 3.63 2.19.45 0 .9-.06 1.35-.27a.6.6 0 00-.21 1.08c1.32.9 3.06.63 4.02-.69.96-1.29.69-3.03-.69-4.02-1.35-.96-3.09-.69-4.02.66a.6.6 0 00.9.96c.6-.63 1.5-.75 2.19-.21.69.51.93 1.41.45 2.1-.48.69-1.38.93-2.1.45a.6.6 0 00-.72.3c-.06.33.15.63.48.69.93.15 1.83-.18 2.49-.81.63-.66.81-1.59.42-2.37a.6.6 0 00-.99-.39c-.6.42-.81 1.2-.51 1.83a.6.6 0 00.96.42z"/></svg>
 )
 
 const GitIcon = () => (
@@ -88,7 +85,7 @@ const GitIcon = () => (
 const DockerIcon = () => (
     <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-full w-full" fill="currentColor">
         <title>Docker</title>
-        <path d="M22.152 7.27c-1.35-1.332-3.21-2.22-5.19-2.22H7.038c-1.98 0-3.84 1.092-5.19 2.22-1.14 1.116-1.848 2.7-1.848 4.392v4.56c0 1.692.708 3.276 1.848 4.392 1.35 1.128 3.21 2.22 5.19 2.22h9.924c1.98 0 3.84-1.092 5.19-2.22 1.14-1.116 1.848-2.7 1.848-4.392v-4.56c0-1.692-.708-3.48-1.848-4.392zm-1.056 8.82c-.9.9-2.316 1.548-3.876 1.548H7.314c-1.56 0-2.976-.648-3.876-1.548a4.108 4.108 0 0 1-1.38-3.264v-1.14c.237-.06.48-.108.708-.18.72-.252 1.417-.588 2.064-.996.36-.228.696-.48.996-.756.24-.216.516-.42.756-.636.228-.204.456-.408.672-.6.204-.192.408-.384.588-.576.228-.24.432-.492.636-.756.192-.252.36-.528.504-.816.144-.288.264-.588.36-.9.084-.3.156-.612.192-.936H11.4v4.5h2.25v-4.5h1.116c.036.324.108.636.192.936.096.312.216.612.36.9.144.288.312.564.504.816.204.264.408.516.636.756.18.192.384.384.588.576.216.192.444.396.672.6.24.216.516.42.756.636.3.276.636.528.996.756.648.408 1.344.744 2.064.996.228.072.471.12.708.18v1.14c0 1.224-.48 2.388-1.38 3.264zM4.5 10.5H3V9h1.5zm3 0H6V9h1.5zm3 0H9V9h1.5zm3 0H12V9h1.5z"/>
+        <path d="M22.152 7.27c-1.35-1.332-3.21-2.22-5.19-2.22H7.038c-1.98 0-3.84 1.092-5.19 2.22-1.14 1.116-1.848 2.7-1.848 4.392v4.56c0 1.692.708 3.276 1.848 4.392 1.35 1.128 3.21 2.22 5.19 2.22h9.924c1.98 0 3.84-1.092 5.19-2.22 1.14-1.116 1.848 2.7 1.848-4.392v-4.56c0-1.692-.708-3.48-1.848-4.392zm-1.056 8.82c-.9.9-2.316 1.548-3.876 1.548H7.314c-1.56 0-2.976-.648-3.876-1.548a4.108 4.108 0 0 1-1.38-3.264v-1.14c.237-.06.48-.108.708-.18.72-.252 1.417-.588 2.064-.996.36-.228.696-.48.996-.756.24-.216.516-.42.756-.636.228-.204.456-.408.672-.6.204-.192.408-.384.588-.576.228-.24.432-.492.636-.756.192-.252.36-.528.504-.816.144-.288.264-.588.36-.9.084-.3.156-.612.192-.936H11.4v4.5h2.25v-4.5h1.116c.036.324.108.636.192.936.096.312.216.612.36.9.144.288.312.564.504.816.204.264.408.516.636.756.18.192.384.384.588.576.216.192.444.396.672.6.24.216.516.42.756.636.3.276.636.528.996.756.648.408 1.344.744 2.064.996.228.072.471.12.708.18v1.14c0 1.224-.48 2.388-1.38 3.264zM4.5 10.5H3V9h1.5zm3 0H6V9h1.5zm3 0H9V9h1.5zm3 0H12V9h1.5z"/>
     </svg>
 )
 
@@ -333,43 +330,43 @@ const skillsData = [
   {
     category: "Programming Languages",
     skills: [
-      { name: 'JavaScript', icon: <JavaScriptIcon /> },
-      { name: 'TypeScript', icon: <TypeScriptIcon /> },
+      { name: 'JavaScript', icon: <JavaScriptIcon />, className: "bg-[#F7DF1E] text-black" },
+      { name: 'TypeScript', icon: <TypeScriptIcon />, className: "bg-[#3178C6] text-white" },
     ],
   },
   {
     category: "Frontend Frameworks & Libraries",
     skills: [
-      { name: 'React', icon: <ReactIcon /> },
-      { name: 'Next.js', icon: <NextjsIcon /> },
-      { name: 'Tailwind CSS', icon: <TailwindCssIcon /> },
+      { name: 'React', icon: <ReactIcon />, className: "text-[#61DAFB]" },
+      { name: 'Next.js', icon: <NextjsIcon />, className: "text-foreground" },
+      { name: 'Tailwind CSS', icon: <TailwindCssIcon />, className: "text-[#38B2AC]" },
     ],
   },
   {
     category: "Backend & Databases",
     skills: [
-      { name: 'Node.js', icon: <NodejsIcon /> },
-      { name: 'Firebase', icon: <FirebaseIcon /> },
-      { name: 'PostgreSQL', icon: <PostgreSqlIcon /> },
-      { name: 'MongoDB', icon: <MongoDbIcon /> },
+      { name: 'Node.js', icon: <NodejsIcon />, className: "text-[#339933]" },
+      { name: 'Firebase', icon: <FirebaseIcon />, className: "text-[#FFCA28]" },
+      { name: 'PostgreSQL', icon: <PostgreSqlIcon />, className: "text-[#4169E1]" },
+      { name: 'MongoDB', icon: <MongoDbIcon />, className: "text-[#47A248]" },
     ],
   },
   {
     category: "Tools & Platforms",
     skills: [
-      { name: 'Git', icon: <GitIcon /> },
-      { name: 'GitHub', icon: <Github className="h-full w-full" /> },
-      { name: 'Docker', icon: <DockerIcon /> },
-      { name: 'Figma', icon: <FigmaIcon /> },
+      { name: 'Git', icon: <GitIcon />, className: "text-[#F05032]" },
+      { name: 'GitHub', icon: <Github className="h-full w-full" />, className: "text-foreground" },
+      { name: 'Docker', icon: <DockerIcon />, className: "text-[#2496ED]" },
+      { name: 'Figma', icon: <FigmaIcon />, className: "" },
     ]
   },
   {
     category: "Soft Skills",
     skills: [
-      { name: 'Communication', icon: <MessageSquareText className="h-full w-full" /> },
-      { name: 'Teamwork', icon: <Users className="h-full w-full" /> },
-      { name: 'Problem Solving', icon: <Puzzle className="h-full w-full" /> },
-      { name: 'Creativity', icon: <Lightbulb className="h-full w-full" /> },
+      { name: 'Communication', icon: <MessageSquareText className="h-full w-full" />, className: "text-primary" },
+      { name: 'Teamwork', icon: <Users className="h-full w-full" />, className: "text-primary" },
+      { name: 'Problem Solving', icon: <Puzzle className="h-full w-full" />, className: "text-primary" },
+      { name: 'Creativity', icon: <Lightbulb className="h-full w-full" />, className: "text-primary" },
     ]
   }
 ];
@@ -412,7 +409,7 @@ function SkillsSection() {
                       <Card 
                         className="flex h-full flex-col items-center justify-center p-4 text-center transition-transform duration-300 hover:-translate-y-2"
                       >
-                        <div className="flex h-16 w-16 items-center justify-center text-primary">
+                        <div className={cn("flex h-16 w-16 items-center justify-center rounded-full p-3", skill.className)}>
                             {skill.icon}
                         </div>
                         <p className="mt-2 text-sm font-semibold">{skill.name}</p>
@@ -470,8 +467,23 @@ function ContactSection() {
         
         <div className="mx-auto mt-12 grid max-w-5xl gap-12 md:grid-cols-2">
             <AnimatedOnScroll
-                classNameIn="animate-in fade-in slide-in-from-left-16 duration-750 delay-200"
+                classNameIn="animate-in fade-in slide-in-from-left-16 duration-750 delay-400"
                 classNameOut="animate-out fade-out slide-out-to-left-16 duration-750 opacity-0"
+            >
+                <Card className="h-full">
+                    <CardHeader>
+                        <CardTitle>Send a Message</CardTitle>
+                        <CardDescription>Fill out the form and I'll get back to you soon.</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <ContactForm />
+                    </CardContent>
+                </Card>
+            </AnimatedOnScroll>
+
+            <AnimatedOnScroll
+                classNameIn="animate-in fade-in slide-in-from-right-16 duration-750 delay-200"
+                classNameOut="animate-out fade-out slide-out-to-right-16 duration-750 opacity-0"
                 className="flex flex-col justify-center space-y-8"
             >
                 <div>
@@ -503,21 +515,6 @@ function ContactSection() {
                         </Button>
                     </div>
                 </div>
-            </AnimatedOnScroll>
-
-            <AnimatedOnScroll
-                classNameIn="animate-in fade-in slide-in-from-right-16 duration-750 delay-400"
-                classNameOut="animate-out fade-out slide-out-to-right-16 duration-750 opacity-0"
-            >
-                <Card className="h-full">
-                    <CardHeader>
-                        <CardTitle>Send a Message</CardTitle>
-                        <CardDescription>Fill out the form and I'll get back to you soon.</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <ContactForm />
-                    </CardContent>
-                </Card>
             </AnimatedOnScroll>
         </div>
       </div>
